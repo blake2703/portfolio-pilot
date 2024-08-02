78da74dd1e1151d1f3e22ecfa86e60700fb06555
blake2703 <dickersonb27@yahoo.com>
2024-08-01 18:14:47 -0700

    Config setup

--- backend/__init__.py
+++ backend/__init__.py
@@ -1,9 +1,13 @@
 from flask import Flask
 from flask_restx import Api
 from .auth.views import auth_namespace
+from .config.config import config_dict
 
-def create_app():
+def create_app(config=config_dict['dev']):
     app = Flask(__name__)
+    
+    
+    app.config.from_object(obj=config)
     
     api = Api(app=app)
     
---
+++ backend/config/config.py
@@ -0,0 +1,19 @@
+import os
+
+class Config:
+    pass
+
+class DevConfig(Config):
+    pass
+
+class TestConfig(Config):
+    pass
+
+class ProdConfig(Config):
+    pass
+
+config_dict = {
+    'dev': DevConfig,
+    'prod': ProdConfig,
+    'test': TestConfig
+}
